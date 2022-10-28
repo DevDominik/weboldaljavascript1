8692bc70a6b23eaa06954dc22c84db5c8c68b89b
DevDominik <71924117+DevDominik@users.noreply.github.com>
2022-10-28 14:02:25 +0200

    Add files via upload

---
+++ órai munka/script.js
@@ -0,0 +1,9 @@
+function onClick() {
+    document.getElementById("visszaigazoloNev").innerHTML = document.getElementById("nev").innerHTML
+    document.getElementById("visszaigazoloHely").innerHTML = document.getElementById("hely").innerHTML
+    document.getElementById("visszaigazoloTelefon").innerHTML = document.getElementById("telefon").innerHTML
+}
+
+function onFocus(o) {
+    
+}
