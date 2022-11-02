0185f788de61ad7ebadc9754be7ce6018329c2d0
DevDominik <kecskes.dominik.hun@gmail.com>
2022-11-02 17:59:23 +0100

    a

--- órai munka/script.js
+++ órai munka/script.js
@@ -1,9 +1,13 @@
 function onClick() {
-    document.getElementById("visszaigazoloNev").innerHTML = document.getElementById("nev").innerHTML
-    document.getElementById("visszaigazoloHely").innerHTML = document.getElementById("hely").innerHTML
-    document.getElementById("visszaigazoloTelefon").innerHTML = document.getElementById("telefon").innerHTML
+    document.getElementById("visszaigazoloNev").innerHTML = document.getElementById("nev").value;
+    document.getElementById("visszaigazoloHely").innerHTML = document.getElementById("hely").value;
+    document.getElementById("visszaigazoloTelefon").innerHTML = document.getElementById("telefon").value;
 }
 
-function onFocus(o) {
-    
+function onFocus(obj) {
+    obj.style.backgroundColor = "rgb(140,123,210)"
+}
+
+function onBlur(obj) {
+    obj.style.backgroundColor = "rgb(255,255,255)"
 }
